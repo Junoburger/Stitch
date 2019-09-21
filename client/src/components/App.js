0bc7bd17a5e53a0c28ac6988f322d324ac2a3f06
JunoJulius <juno.burger@officebooking.net>
2019-09-21 16:07:02 +0200

    added RTMProtocol

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,29 +1,30 @@
-import React from 'react';
-import { Router, Route } from 'react-router-dom';
-import StreamCreate from './streams/StreamCreate';
-import StreamEdit from './streams/StreamEdit';
-import StreamDelete from './streams/StreamDelete';
-import StreamList from './streams/StreamList';
-import StreamShow from './streams/StreamShow';
-import Header from './Header'
-import createBrowserHistory from '../history'
+import React from "react";
+import { Router, Route, Switch } from "react-router-dom";
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
+import createBrowserHistory from "../history";
 
 const App = () => {
-    return (
-        <div className="ui container">
-    <Router history={createBrowserHistory}>
-            <div>
-        <Header/>
-                <Route path="/" exact component={StreamList}/>
-                <Route path="/streams/new" exact component={StreamCreate}/>
-                <Route path="/streams/edit/:id" exact component={StreamEdit}/>
-                <Route path="/streams/delete/:id" exact component={StreamDelete}/>
-                <Route path="/streams/show" exact component={StreamShow}/>
-            </div>
-            </Router>
+  return (
+    <div className="ui container">
+      <Router history={createBrowserHistory}>
+        <div>
+          <Header />
+          <Switch>
+            <Route path="/" exact component={StreamList} />
+            <Route path="/streams/new" exact component={StreamCreate} />
+            <Route path="/streams/edit/:id" exact component={StreamEdit} />
+            <Route path="/streams/delete/:id" exact component={StreamDelete} />
+            <Route path="/streams/:id" exact component={StreamShow} />
+          </Switch>
         </div>
-    )
+      </Router>
+    </div>
+  );
 };
 
-
-export default App
+export default App;
--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -15,8 +15,11 @@ class StreamList extends React.Component {
           <Link to={`/streams/edit/${stream.id}`} className="ui button primary">
             Edit
           </Link>
-          <Link to={`/streams/delete/${stream.id}`} className="ui button negative">
-          Delete
+          <Link
+            to={`/streams/delete/${stream.id}`}
+            className="ui button negative"
+          >
+            Delete
           </Link>
         </div>
       );
@@ -30,7 +33,9 @@ class StreamList extends React.Component {
           {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">
-            {stream.title}
+            <Link to={`/streams/${stream.id}`} className="header">
+              {stream.title}
+            </Link>
             <div className="description">{stream.description}</div>
           </div>
         </div>
